815b79a1994b053e37dda764177c7b4a288fb222
aspectZA27 <ruakruger@gmail.com>
2026-03-31 21:39:11 +0200

    Trying to fix addbook function

--- index.js
+++ index.js
@@ -8,12 +8,32 @@ const allBooks = [
     Summary: "",
   },
 ];
-
-const books = document.getElementById("books");
-
-const addbook = document.getElementById("addbook");
+const form = document.getElementById("form");
+form.addEventListener("submit", addBook);
 
 function popup() {
-  var popup = document.getElementById("popup-form");
+  var popup = document.getElementById("form");
   popup.classList.toggle("show");
+}
+//All inputs
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const publicationYear = document.getElementById("publication-year");
+const genre = document.getElementById("genre");
+const summary = document.getElementById("summary");
+
+//Function to add new books
+function addBook(event) {
+  event.preventDefault();
+  const newBook = {
+    id: Math.floor(Math.random() * 1000),
+    Title: title,
+    Author: author,
+    PublicationYear: publicationYear,
+    Genre: genre,
+    Summary: summary,
+  };
+
+  allBooks.push(newBook);
+  console.log(allBooks);
 }
